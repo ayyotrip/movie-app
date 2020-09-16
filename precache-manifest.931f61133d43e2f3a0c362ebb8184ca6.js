62a704dcaded478e4484a8394643c4b72d132c8e
J.P. Lee <jplee@goodnews.or.kr>
2020-09-16 13:39:55 +0900

    Updates

---
+++ precache-manifest.931f61133d43e2f3a0c362ebb8184ca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf2e6c2a477b2da7de5b812e2a9f36f",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "2f8e292e5d97a9c09954",
+    "url": "/movie-app/static/css/main.9fb02879.chunk.css"
+  },
+  {
+    "revision": "1c1867ee385e1fd15da6",
+    "url": "/movie-app/static/js/2.3a05f077.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.3a05f077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f8e292e5d97a9c09954",
+    "url": "/movie-app/static/js/main.39517890.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
